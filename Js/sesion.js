6d7da9501c307e88c0f27afc4e2fa683cd598565
Andres Felipe Leon <afleon386@misena.edu.co>
2022-07-31 15:51:29 -0500

    validación de ingreso

--- Js/sesion.js
+++ Js/sesion.js
@@ -8,6 +8,13 @@ $(document).ready(function(){
     }).done(function(json){
         console.log('entraste')
         console.log(json)
+    if(json !=0){
+        window.location =  "vista/inicio.php";
+        alert('bienvenido')
+    }
+    else{
+        alert('error')
+    }
     }).fail(function(error){
         console.log('error')
     })
