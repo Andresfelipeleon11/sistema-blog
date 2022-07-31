8778ec741de0a7bd8bee4a035e3bb0d53ffb173a
Andres Felipe Leon <afleon386@misena.edu.co>
2022-07-31 15:39:52 -0500

    nuevo script
    para validar la session

---
+++ Js/validarSesion.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){+
+    validarSesion();
+    function validarSesion(){
+        $.ajax({
+            url: './controlador/sesionBuscar.php',
+            method: 'POST',
+            dataType: 'JSON',
+            data: null
+        }).done(function(json){
+            if (json==0){
+                window.location.href = './index.html';
+            }
+        }).fail(function(){})
+    }
+})
