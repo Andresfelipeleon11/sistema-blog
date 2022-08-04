af242020bc4151c0d61fbce65322946748b910e4
Andres Felipe Leon <afleon386@misena.edu.co>
2022-08-03 20:11:34 -0500

    creacion de entidad blog
    con las tablas del phpMyadmin

---
+++ Entidad/blogEntidad.php
@@ -0,0 +1,71 @@
+<?php
+namespace Entidad;
+
+class Blog{
+    private $idBlog;
+    private $mensaje;
+    private $idUsuario;
+
+    /**
+     * Get the value of idBlog
+     */ 
+    public function getIdBlog()
+    {
+        return $this->idBlog;
+    }
+
+    /**
+     * Set the value of idBlog
+     *
+     * @return  self
+     */ 
+    public function setIdBlog($idBlog)
+    {
+        $this->idBlog = $idBlog;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of mensaje
+     */ 
+    public function getMensaje()
+    {
+        return $this->mensaje;
+    }
+
+    /**
+     * Set the value of mensaje
+     *
+     * @return  self
+     */ 
+    public function setMensaje($mensaje)
+    {
+        $this->mensaje = $mensaje;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idUsuario
+     */ 
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * Set the value of idUsuario
+     *
+     * @return  self
+     */ 
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
+
+        return $this;
+    }
+}
+
+
+?>
